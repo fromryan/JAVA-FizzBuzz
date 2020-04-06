f26c874d660626a9246b90617c47ce04d2f51dc1
fromryan <johndoe@example.com>
2020-04-06 16:41:18 -0700

    initial commit

---
+++ FizzBuzzTest.java
@@ -0,0 +1,9 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz iD = new FizzBuzz();
+        iD.fizzBuzz(3);
+        iD.fizzBuzz(5);
+        iD.fizzBuzz(15);
+        iD.fizzBuzz(2);
+    }
+}
